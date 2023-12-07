edd323827a84e51b3f13e1e79dac1ffb01c8f872
Jey223 <julieudoh7@gmail.com>
2023-12-06 21:38:51 -0800

    make toggle switchable on click of parent div

--- file.js
+++ file.js
@@ -1,44 +1,21 @@
+const toggleDiv = document.getElementById('toggle-div');
 const toggle = document.getElementById('toggle');
+
 
 const h2Tags = document.getElementsByTagName('h2');
 
 
-let elemntClicked = false;
 
 const toggleContent = () => {
-    // const toggleContent = (e) => {
-
-    
-        // console.log(e)
-        // if(e.type == "click" && e.key == "Enter") {
-        //     let newToggle = toggleHolder.classList.toggle('active');
-        //     toggle.classList.toggle('toggle-active');
-
-            
-        //     for(let i = 0;i < h2Tags.length; i++) {
-        //         console.log(h2Tags[i])
-        //         h2Tags[i].classList.toggle('monthly-usd')
-        //     }
-                
-        // }
-    // }
         toggle.classList.toggle('active');
 
         
         for(let i = 0;i < h2Tags.length; i++) {
             h2Tags[i].classList.toggle('monthly-usd')
         }
-        
-    
 }
 
 
 
-// ["click", "keydown"].forEach(ev => {
-//     toggle.addEventListener("click", toggleContent);
-// });
 
-toggle.addEventListener("click", toggleContent);
-
-
-// toggle.addEventListener("click", toggleContent);
+toggleDiv.addEventListener("click", toggleContent);
